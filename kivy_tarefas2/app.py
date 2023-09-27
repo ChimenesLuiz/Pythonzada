dbb0305e33a5b7b39d2219e99f00b78498bfc20a
Luiz Guilherme Chimenes <122306667+ChimenesLuiz@users.noreply.github.com>
2023-09-27 16:57:59 -0400

    AA

--- kivy_tarefas2/app.py
+++ kivy_tarefas2/app.py
@@ -1,84 +1,83 @@
-from kivy.metrics import dp
+from kivy.lang import Builder
 
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.app import MDApp
-from kivymd.uix.datatables import MDDataTable
-from kivymd.uix.screen import MDScreen
+
+KV = '''
+MDScreen:
+
+    MDNavigationLayout:
+
+        MDScreenManager:
+
+            MDScreen:
+
+                MDRaisedButton:
+                    on_press: Main.POST()
+                    text: "Cadastrar"
+                    md_bg_color: "green"
+                    pos_hint: {"center_x": .5, "center_y": .45}
+                    
+                    
+                MDTextField:
+                    id: data
+                    hint_text: "Data para conclusao"
+                    helper_text: "There will always be a mistake"
+                    helper_text_mode: "on_error"
+                    pos_hint: {"center_x": .5, "center_y": .55}
+                    size_hint_x: .5
+                    
+                MDTextField:
+                    id: descricao
+                    hint_text: "Descricao"
+                    helper_text: "There will always be a mistake"
+                    helper_text_mode: "on_error"
+                    pos_hint: {"center_x": .5, "center_y": .65}
+                    size_hint_x: .5
+                    
+                MDTextField:
+                    id: nome_tarefa
+                    hint_text: "Nome Tarefa"
+                    helper_text: "There will always be a mistake"
+                    helper_text_mode: "on_error"
+                    pos_hint: {"center_x": .5, "center_y": .75}
+                    size_hint_x: .5
+                
+                MDTopAppBar:
+                    title: "Navigation Drawer"
+                    elevation: 4
+                    pos_hint: {"top": 1}
+                    md_bg_color: "#e7e4c0"
+                    specific_text_color: "#4a4939"
+                    left_action_items:
+                        [['menu', lambda x: nav_drawer.set_state("open")]]
 
 
-class Example(MDApp):
+        MDNavigationDrawer:
+            id: nav_drawer
+            radius: (0, 16, 16, 0)
+
+            ContentNavigationDrawer:
+'''
+
+
+class ContentNavigationDrawer(MDBoxLayout):
+    pass
+#self.root.ids.$variavel
+
+class Main(MDApp):
     def build(self):
-        self.data_tables = MDDataTable(
-            use_pagination = True,
-            check = True,
-            column_data=[
-                ("Nome", dp(35)),
-                ("Descricao", dp(35)),
-                ("Data Limite", dp(60)),
-                ("Status", dp(35))
-            ],
-            row_data=[
-                (
-                    "Passeio",
-                    "Passear com meu cachorro rex na praca",
-                    "2023/10/10",
-                    "A fazer"
-                )
-            ],
-            sorted_on = "Schedule",
-            sorted_order = "ASC",
-            elevation = 2,
-        )
-        
-        self.data_tables.bind(on_row_press = self.on_row_press)
-        self.data_tables.bind(on_check_press = self.on_check_press)
-        screen = MDScreen()
-        screen.add_widget(self.data_tables)
-        return screen
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_string(KV)
+    
+    def POST(self):
+        dados = {"nome": self.root.ids.nome_tarefa,
+                "descricao": self.root.ids.descricao,
+                "data_tarefa": self.root.ids.data_tarefa,
+                "estado_tarefa": "F"}
 
-    def on_row_press(self, instance_table, instance_row):
-        '''Called when a table row is clicked.'''
-
-        print(instance_table, instance_row)
-
-    def on_check_press(self, instance_table, current_row):
-        '''Called when the check box in the table row is checked.'''
-
-        print(instance_table, current_row)
-
-    # Sorting Methods:
-    # since the https://github.com/kivymd/KivyMD/pull/914 request, the
-    # sorting method requires you to sort out the indexes of each data value
-    # for the support of selections.
-    #
-    # The most common method to do this is with the use of the builtin function
-    # zip and enumerate, see the example below for more info.
-    #
-    # The result given by these funcitons must be a list in the format of
-    # [Indexes, Sorted_Row_Data]
-
-    def sort_on_signal(self, data):
-        return zip(*sorted(enumerate(data), key = lambda l: l[1][2]))
-
-    def sort_on_schedule(self, data):
-        return zip(
-            *sorted(
-                enumerate(data),
-                key = lambda l: sum(
-                    [
-                        int(l[1][-2].split(":")[0]) * 60,
-                        int(l[1][-2].split(":")[1]),
-                    ]
-                ), 
-            )
-        )
-
-    def sort_on_team(self, data):
-        return zip(*sorted(enumerate(data), key=lambda l: l[1][-1]))
+        print(dados)
+    
 
 
-Example().run()
-# from entity.Tarefa import Tarefa
-
-# tk = Tarefa()
-# dados = tk.getDados()
-# print(dados)
+Main().run()
--- kivy_tarefas2/entity/Tarefa.py
+++ kivy_tarefas2/entity/Tarefa.py
@@ -1,20 +1,2 @@
-from entity.Db import Database
 
-class Tarefa:
-    def __init__(self) -> None:
-        pass
-    
-    
-    def cadastrar(dados_p = dict) -> bool:
-        db = Database('luiz')
-        
-        db.insert(dados = dados_p)
-        return True
-    
-    
-    def getDados(self) -> list:
-        db = Database('luiz')
-        
-        dados = db.select()
-        return dados
 
