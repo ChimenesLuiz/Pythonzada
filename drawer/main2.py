fe186fdc16b6c678840c96bc0679e84105fdca55
Luiz Guilherme Chimenes <Luizguichimenes2002@gmail.com>
2023-10-18 22:28:14 -0400

    sad
    sad

---
+++ drawer/main2.py
@@ -0,0 +1,19 @@
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+
+from kivymd.app import MDApp
+from kivymd.uix.scrollview import MDScrollView
+
+class ContentNavigationDrawer(MDScrollView):
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
+
+
+class Example(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "Orange"
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_file("main.kv")
+
+
+Example().run()
